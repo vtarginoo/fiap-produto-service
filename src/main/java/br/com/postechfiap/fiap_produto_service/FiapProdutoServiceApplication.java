728292eec5091fac3475a28d47b531b5fc250b83
vtarginoo <vtarginoo>
2025-04-02 16:22:05 -0300

    first commit

---
+++ src/main/java/br/com/postechfiap/fiap_produto_service/FiapProdutoServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.postechfiap.fiap_produto_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapProdutoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapProdutoServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/postechfiap/fiap_produto_service/FiapProdutoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.postechfiap.fiap_produto_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapProdutoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

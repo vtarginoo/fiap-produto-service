b57f587d9d4f94c59dbd83cae12ec585ced438c8
vtarginoo <vtarginoo>
2025-04-11 10:09:49 -0300

    Bugfix

--- src/main/java/br/com/postechfiap/fiap_produto_service/interfaces/client/EstoqueClient.java
+++ src/main/java/br/com/postechfiap/fiap_produto_service/interfaces/client/EstoqueClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "estoqueClient", url = "http://localhost:8081")
+@FeignClient(name = "estoqueClient", url = "http://localhost:8082")
 public interface EstoqueClient {
 
     @PostMapping("/estoque")

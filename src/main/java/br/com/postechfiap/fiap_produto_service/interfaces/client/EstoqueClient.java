b283f524f02bbbfd8e955307a59b5e05d733a12c
thiagomagdalena <thiagomagdalena@gmail.com>
2025-05-13 10:51:27 -0300

    correções

--- src/main/java/br/com/postechfiap/fiap_produto_service/interfaces/client/EstoqueClient.java
+++ src/main/java/br/com/postechfiap/fiap_produto_service/interfaces/client/EstoqueClient.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "estoqueClient", url = "http://localhost:8082")
+@FeignClient(name = "estoqueClient", url = "${client.estoque-service.url}")
 public interface EstoqueClient {
 
     @PostMapping("/estoque")
